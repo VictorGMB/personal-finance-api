ff7923f96479d6d4e04629a1538278b29688b5a2
VictorGMB <victormedeiros2580@hotmail.com>
2025-06-08 00:41:56 -0300

    Codificação da Classe User

--- PersonalFinance.Api/PersonalFinance.Api/Models/User.cs
+++ PersonalFinance.Api/PersonalFinance.Api/Models/User.cs
@@ -1,6 +1,15 @@
-﻿namespace PersonalFinance.Api.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PersonalFinance.Api.Models
 {
     public class User
     {
+        [Key]
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public required string PasswordHash { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
     }
 }
